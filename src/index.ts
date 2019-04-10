7d5b179637ad94fcb02071a0948a5afb0a3d3945
nmearl <contact@nicholasearl.me>
2019-04-10 09:48:13 -0400

    Create initial show command

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,13 @@ import {
 
 import '../style/index.css';
 
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
+  Widget
+} from '@phosphor/widgets';
 
 /**
  * Initialization data for the cube_lab extension.
@@ -11,8 +18,33 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'cube_lab',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension cube_lab is activated!');
+
+    let widget: Widget = new Widget();
+    widget.id = 'cube-lab-palette';
+    widget.title.label = 'CubeLab';
+    widget.title.closable = true;
+
+    const command: string = 'cube-lab:open';
+
+    app.commands.addCommand(command, {
+      label: 'New Tab',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not already there
+          app.shell.addToMainArea(widget);
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette
+    palette.addItem({command, category: 'CubeLab'});
+
+    console.log('ICommandPalette:', palette)
   }
 };
 
